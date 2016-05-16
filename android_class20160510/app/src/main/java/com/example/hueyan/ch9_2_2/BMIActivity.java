eefb6bab04d2d886c1ee915c1f095c6a3ee3787b
Yu-Che-Gao <s0258037@gm.csmu.edu.tw>
2016-05-17 01:41:56 +0800

    20160517
    完成201160517作業

--- android_class20160510/app/src/main/java/com/example/hueyan/ch9_2_2/BMIActivity.java
+++ android_class20160510/app/src/main/java/com/example/hueyan/ch9_2_2/BMIActivity.java
@@ -20,20 +20,36 @@ public class BMIActivity extends Activity {
     }
 
     private double[] getFromBundle(Bundle bundle) {
-        double[] returnNumber = new double[2];
+        double[] returnNumber = new double[4];
         returnNumber[0] = Double.parseDouble(bundle.getString("HEIGHT"));
         returnNumber[1] = Double.parseDouble(bundle.getString("WEIGHT"));
+        returnNumber[2] = Double.parseDouble(bundle.getString("AGE"));
+        if (bundle.getString("SEX").equals("男")) {
+            returnNumber[3] = 0;
+        } else {
+            returnNumber[3] = 1;
+        }
         return returnNumber;
     }
 
     private void displayBMI() {
-        double height, weight, bmi;
+        double height, weight, age, bmi;
+        String sex = "", result;
         Bundle bundle = this.getIntent().getExtras();
         if (bundle != null) {
             height = getFromBundle(bundle)[0];
             weight = getFromBundle(bundle)[1];
+            //TODO 加入年齡性別
+            age = getFromBundle(bundle)[2];
+            if (getFromBundle(bundle)[3] == 0) {
+                sex = "男";
+            } else {
+                sex = "女";
+            }
+
             bmi = calculateBMI(weight, height);
-            showBMI("BMI值: " + Double.toString(bmi));
+            result = attributeBMI(bmi, (int) age, sex);
+            showBMI("BMI值: " + Double.toString(bmi) + "\n" + "計算結果:" + result);
         }
     }
 
@@ -48,26 +64,49 @@ public class BMIActivity extends Activity {
         output.setText(string);
     }
 
-    private String attributeBMI(double BMI) {
+    private String attributeBMI(double BMI, int year, String sex) {
         String result = "";
-        String[] rule=
-                {"２歲", "15.2~17.7", "17.7", "19.0", "14.9~17.3", "17.3", "18.3",
-                "３歲", "14.8~17.7", "17.7", "19.1", "14.5~17.2", "17.2", "18.5",
-                "４歲", "14.4~17.7", "17.7", "19.3", "14.2~17.1", "17.1", "18.6",
-                "５歲", "14.0~17.7", "17.7", "19.4", "13.9~17.1", "17.1", "18.9",
-                "６歲", "13.9~17.9", "17.9", "19.7", "13.6~17.2", "17.2", "19.1",
-                "７歲", "14.7~18.6", "18.6", "21.2", "14.4~18.0", "18.0", "20.3",
-                "８歲", "15.0~19.3", "19.3", "22.0", "14.6~18.8", "18.8", "21.0",
-                "９歲", "15.2~19.7", "19.7", "22.5", "14.9~19.3", "19.3", "21.6",
-                "１０歲", "15.4~20.3", "20.3", "22.9", "15.2~20.1", "20.1", "22.3",
-                "１１歲", "15.8~21.0", "21.0", "23.5", "15.8~20.9", "20.9", "23.1",
-                "１２歲", "16.4~21.5", "21.5", "24.2", "16.4~21.6", "21.6", "23.9",
-                "１３歲", "17.0~22.2", "22.2", "24.8", "17.0~22.2", "22.2", "24.6",
-                "１４歲", "17.6~22.7", "22.7", "25.2", "17.6~22.7", "22.7", "25.1",
-                "１５歲", "18.2~23.1", "23.1", "25.5", "18.0~22.7", "22.7", "25.3",
-                "１６歲", "18.6~23.4", "23.4", "25.6", "18.2~22.7", "22.7", "25.3",
-                "１７歲", "19.0~23.6", "23.6", "25.6", "18.3~22.7", "22.7", "25.3",
-                "１８歲", "19.2~23.7", "23.7", "25.6", "18.3~22.7", "22.7", "25.3"};//前三項: 男生, 後三項: 女生
+        String[][] rule =
+                {{"２歲", "15.2~17.7", "14.9~17.3"},
+                        {"３歲", "14.8~17.7", "14.5~17.2"},
+                        {"４歲", "14.4~17.7", "14.2~17.1"},
+                        {"５歲", "14.0~17.7", "13.9~17.1"},
+                        {"６歲", "13.9~17.9", "13.6~17.2"},
+                        {"７歲", "14.7~18.6", "14.4~18.0"},
+                        {"８歲", "15.0~19.3", "14.6~18.8"},
+                        {"９歲", "15.2~19.7", "14.9~19.3"},
+                        {"１０歲", "15.4~20.3", "15.2~20.1"},
+                        {"１１歲", "15.8~21.0", "15.8~20.9"},
+                        {"１２歲", "16.4~21.5", "16.4~21.6"},
+                        {"１３歲", "17.0~22.2", "17.0~22.2"},
+                        {"１４歲", "17.6~22.7", "17.6~22.7"},
+                        {"１５歲", "18.2~23.1", "18.0~22.7"},
+                        {"１６歲", "18.6~23.4", "18.2~22.7"},
+                        {"１７歲", "19.0~23.6", "18.3~22.7"},
+                        {"１８歲", "19.2~23.7", "18.3~22.7"}};
+        if (sex.equals("男")) {
+            String sexRule = rule[year - 2][1];
+            double lowerBound = Double.parseDouble(sexRule.split("~")[0]);
+            double upperBond = Double.parseDouble(sexRule.split("~")[1]);
+            if (BMI < lowerBound) {
+                result = "過瘦";
+            } else if (BMI >= lowerBound && BMI <= upperBond) {
+                result = "正常";
+            } else {
+                result = "過胖";
+            }
+        } else {
+            String sexRule = rule[year - 2][2];
+            double lowerBound = Double.parseDouble(sexRule.split("~")[0]);
+            double upperBond = Double.parseDouble(sexRule.split("~")[1]);
+            if (BMI < lowerBound) {
+                result = "過瘦";
+            } else if (BMI >= lowerBound && BMI <= upperBond) {
+                result = "正常";
+            } else {
+                result = "過胖";
+            }
+        }
         return result;
     }
 }
--- android_class20160510/app/src/main/java/com/example/hueyan/ch9_2_2/MainActivity.java
+++ android_class20160510/app/src/main/java/com/example/hueyan/ch9_2_2/MainActivity.java
@@ -2,30 +2,39 @@ package com.example.hueyan.ch9_2_2;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
+
+    String sex = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // 找出Button元件
         Button btn = (Button) findViewById(R.id.button);
+        RadioGroup genderGroup = (RadioGroup) findViewById(R.id.genderGroup);
         // 註冊傾聽者物件
         btn.setOnClickListener(listener);
+        genderGroup.setOnCheckedChangeListener(checkedChangeListener);
     }
+
     // 建立傾聽者物件
     View.OnClickListener listener = new View.OnClickListener() {
         public void onClick(View v) {
             // 取得EditText元件
             EditText height = (EditText) findViewById(R.id.txtHeight);
             EditText weight = (EditText) findViewById(R.id.txtWeight);
+            EditText ageEdit = (EditText) findViewById(R.id.ageEdit);
             // 建立Intent物件
             Intent myIntent = new Intent();
             myIntent.setClass(MainActivity.this, BMIActivity.class);
@@ -33,8 +42,24 @@ public class MainActivity extends Activity {
             Bundle bundle = new Bundle();
             bundle.putString("HEIGHT", height.getText().toString());
             bundle.putString("WEIGHT", weight.getText().toString());
+            bundle.putString("AGE", ageEdit.getText().toString());
+            bundle.putString("SEX", sex);
             myIntent.putExtras(bundle);  // 加上資料
             startActivity(myIntent);     // 啟動活動
+        }
+    };
+
+    RadioGroup.OnCheckedChangeListener checkedChangeListener = new RadioGroup.OnCheckedChangeListener() {
+        @Override
+        public void onCheckedChanged(RadioGroup group, int checkedId) {
+            switch (checkedId) {
+                case R.id.radioMale:
+                    sex = "男";
+                    break;
+                case R.id.radioFemale:
+                    sex = "女";
+                    break;
+            }
         }
     };
 }
